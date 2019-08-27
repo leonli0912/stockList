a4e98ad94d51083c307f614014c59912447b7ea2
yy <yy@yydeMacBook-Pro.local>
2019-08-28 00:33:04 +0800

    add table page

---
+++ pages/table.vue
@@ -0,0 +1,36 @@
+<template>
+    <vuetable ref="vuetable"
+    api-url="http://localhost:5000/allDividend?year=2019"
+    :fields="['stockId','reportYear','dividendDate','divdendContent']"
+    data-path=""
+    pagination-path="">
+    </vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2'
+
+function setState(store) {}
+
+export default {
+    name: 'table',
+    components:{
+        Vuetable
+    }
+};
+</script>
+
+<style lang="stylus" scoped>
+
+.page-index
+    display flex
+    justify-content center
+    align-items center
+    flex-direction column
+
+    h2
+        font-size 46px
+        font-weight 500
+        margin-bottom 0
+
+</style>
